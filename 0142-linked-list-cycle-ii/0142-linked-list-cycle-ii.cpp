621ae7279f8b625f4ba46bbc8bf92f973da27168
Faysoula <127143975+Faysoula@users.noreply.github.com>
2023-10-06 19:00:59 +0300

    Time: 9 ms (36.97%), Space: 8 MB (24.7%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -0,0 +1,59 @@
+/**
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *int val;
+ *ListNode * next;
+ *ListNode(int x) : val(x), next(NULL) {}
+ *};
+ */
+class Solution
+{
+    private:
+        ListNode* floydDetect(ListNode *head)
+        {
+            if (head == NULL)
+            {
+                return NULL;
+            }
+            ListNode *fast = head;
+            ListNode *slow = head;
+
+            while (slow != NULL && fast != NULL)
+            {
+                fast = fast->next;
+                if (fast != NULL)
+                {
+                    fast = fast->next;
+                }
+                slow = slow->next;
+
+                if (slow == fast)
+                {
+                    return slow;
+                }
+            }
+            return NULL;
+        }
+
+    public:
+        ListNode* detectCycle(ListNode *head)
+        {
+            if (head == NULL)
+            {
+                return NULL;
+            }
+            ListNode *intersect = floydDetect(head);
+            ListNode *slow = head;
+
+            while (slow != intersect)
+            {
+                slow = slow -> next;
+
+                if (intersect != NULL)
+                {
+                    intersect = intersect->next;
+                }
+            }
+                return slow;
+        }
+};
