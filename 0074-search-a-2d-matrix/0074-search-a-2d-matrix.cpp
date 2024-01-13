57a8fd6f51a20e14c3089041589aed5af9e4c77a
Faysal elestwani <127143975+Faysoula@users.noreply.github.com>
2024-01-13 20:09:32 +0200

    Update 0074-search-a-2d-matrix.cpp

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for(int row =0; row<matrix.size();row++){
-            for(int col = 0;col<matrix[row].size();col++){
-                if(matrix[row][col] == target){
-                    return true;
-                }
-            }
+        int row = 0;
+        int col = matrix[0].size() -1;
+
+        while(row< matrix.size() && col > -1){
+            int curr = matrix[row][col];
+            if(curr == target){return true;}
+            if(target > curr){
+                row++;
+            }else col--;
         }
         return false;
     }
