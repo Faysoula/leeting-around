266b66c2208de1fe95a4162188a5c75229be230c
Faysal elestwani <127143975+Faysoula@users.noreply.github.com>
2024-10-20 21:34:23 +0300

    Time: 0 ms (100.00%) | Memory: 8.1 MB (90.99%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res;
+        vector<int> prevRow;
+
+        for(int i = 0; i< numRows;i++){
+            vector<int> currRow(i+1,1); //(fill it with ones)
+
+            for(int j = 1;j<i;j++){
+                currRow[j] = prevRow[j-1] + prevRow[j];
+            }
+
+            res.push_back(currRow);
+            prevRow = currRow;
+        }
+        return res;
+    }
+};
